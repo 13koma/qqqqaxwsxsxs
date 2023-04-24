8b6e2fb303a9875308a42430cc275e4007e8bdbd
13koma <91424950+13koma@users.noreply.github.com>
2023-04-24 14:07:41 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,30 +2,27 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/i2c.h"
-#include "esp_log.h"
 
-#define MPU9150_ADDR   0x68
-#define AK8975_ADDR    0x0C
-#define SDA_GPIO_NUM   21
-#define SCL_GPIO_NUM   22
-#define I2C_FREQ_HZ    100000
+#define MPU9150_ADDR 0x68
+#define AK8975_ADDR  0x0C
 
-static const char* TAG = "GIMBAL";
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} mpu9150_mag_data_t;
 
-static esp_err_t i2c_init() {
+void i2c_init() {
     i2c_config_t conf;
     conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = SDA_GPIO_NUM;
-    conf.scl_io_num = SCL_GPIO_NUM;
+    conf.sda_io_num = GPIO_NUM_21;
+    conf.scl_io_num = GPIO_NUM_22;
     conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
     conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.master.clk_speed = I2C_FREQ_HZ;
+    conf.master.clk_speed = 100000;
     conf.clk_flags = 0;
-    esp_err_t ret = i2c_param_config(I2C_NUM_0, &conf);
-    if (ret != ESP_OK) {
-        return ret;
-    }
-    return i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0);
+    i2c_param_config(I2C_NUM_0, &conf);
+    i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0);
 }
 
 void initialize_mpu9150() {
@@ -42,7 +39,7 @@ void initialize_mpu9150() {
     i2c_cmd_link_delete(cmd);
 }
 
-void read_magnetometer(int16_t *mx, int16_t *my, int16_t *mz) {
+void read_magnetometer(mpu9150_mag_data_t *mag_data) {
     uint8_t data[6];
     i2c_cmd_handle_t cmd;
 
@@ -70,23 +67,20 @@ void read_magnetometer(int16_t *mx, int16_t *my, int16_t *mz) {
     i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
 
-    *mx = (data[1] << 8) | data[0];
-    *my = (data[3] << 8) | data[2];
-    *mz = (data[5] << 8) | data[4];
+    mag_data->x = (data[1] << 8) | data[0];
+    mag_data->y = (data[3] << 8) | data[2];
+    mag_data->z = (data[5] << 8) | data[4];
 }
 
 void mpu9150_task(void *pvParameters) {
-    ESP_LOGI(TAG, "I2C INITIALIZATION STARTED");
+    mpu9150_mag_data_t mag_data;
     i2c_init();
-    ESP_LOGI(TAG, "I2C INITIALIZATION SUCCESSFUL");
     initialize_mpu9150();
 
     while (1) {
-        int16_t mx, my, mz;
+        read_magnetometer(&mag_data);
 
-        read_magnetometer(&mx, &my, &mz);
-
-        printf("Mag: X=%d, Y=%d, Z=%d\n", mx, my, mz);
+        printf("Mag: X=%d, Y=%d, Z=%d\n", mag_data.x, mag_data.y, mag_data.z);
 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
